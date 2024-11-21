83227b087782dfb48a36ef47e0c0ee94945d973f
Davide Delogu <davide.delogu@itiscuneo.eu>
2024-11-21 12:07:07 +0100

    Working

--- client.py
+++ client.py
@@ -5,60 +5,52 @@ SERVER_ADDRESS = ('localhost', 12345)
 BUFFER_SIZE = 1024
 
 def send_request(request):
+    """Invia una richiesta al server e restituisce la risposta."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect(SERVER_ADDRESS)
         client_socket.sendall(request.encode())
         response = client_socket.recv(BUFFER_SIZE).decode()
         return response
 
-# Esempi di richieste
+def main():
+    """Funzione principale per gestire il menu del client."""
+    while True:
+        print("\nMenu:")
+        print("1. Controlla se un file esiste")
+        print("2. Ottieni il numero di frammenti di un file")
+        print("3. Ottieni l'IP di un frammento specifico")
+        print("4. Ottieni tutti gli IP dei frammenti di un file")
+        print("5. Esci")
+
+        choice = input("Scegli un'opzione (1-5): ")
+
+        if choice == '1':
+            file_name = input("Inserisci il nome del file: ")
+            response = send_request(f'CHECK_FILE|{file_name}')
+            print(response)
+
+        elif choice == '2':
+            file_name = input("Inserisci il nome del file: ")
+            response = send_request(f'GET_FRAGMENT_COUNT|{file_name}')
+            print(response)
+
+        elif choice == '3':
+            file_name = input("Inserisci il nome del file: ")
+            fragment_number = input("Inserisci il numero del frammento: ")
+            response = send_request(f'GET_FRAGMENT_IP|{file_name}|{fragment_number}')
+            print(response)
+
+        elif choice == '4':
+            file_name = input("Inserisci il nome del file: ")
+            response = send_request(f'GET_ALL_FRAGMENT_IPS|{file_name}')
+            print(response)
+
+        elif choice == '5':
+            print("Uscita dal client.")
+            break
+
+        else:
+            print("Opzione non valida. Riprova.")
+
 if __name__ == "__main__":
-    # Controlla se un file esiste
-    response = send_request('CHECK_FILE|dune.mov')
-    print(response)
-
-    # Ottieni il numero di frammenti di un file
-    response = send_request('GET_FRAGMENT_COUNT|dune.mov')
-    print(response)
-
-    # Ottieni l'IP di un frammento specifico
-    response = send_request('GET_FRAGMENT_IP|dune.mov|1')
-    print(response)
-
-    # Ottieni tutti gli IP dei frammenti di un file
-    response = send_request('GET_ALL_FRAGMENT_IPS|dune.mov')
-    print(response)
-
-    print("---------------")
-
-    response = send_request('CHECK_FILE|ciclofondazione_asimov.djvu')
-    print(response)
-
-    # Ottieni il numero di frammenti di un file
-    response = send_request('GET_FRAGMENT_COUNT|ciclofondazione_asimov.djvu')
-    print(response)
-
-    # Ottieni l'IP di un frammento specifico
-    response = send_request('GET_FRAGMENT_IP|ciclofondazione_asimov.djvu|1')
-    print(response)
-
-    # Ottieni tutti gli IP dei frammenti di un file
-    response = send_request('GET_ALL_FRAGMENT_IPS|ciclofondazione_asimov.djvu')
-    print(response)
-
-    print("---------------")
-
-    response = send_request('CHECK_FILE|whatif_munroe.epub')
-    print(response)
-
-    # Ottieni il numero di frammenti di un file
-    response = send_request('GET_FRAGMENT_COUNT|whatif_munroe.epub')
-    print(response)
-
-    # Ottieni l'IP di un frammento specifico
-    response = send_request('GET_FRAGMENT_IP|whatif_munroe.epub|1')
-    print(response)
-
-    # Ottieni tutti gli IP dei frammenti di un file
-    response = send_request('GET_ALL_FRAGMENT_IPS|whatif_munroe.epub')
-    print(response)
+    main()
